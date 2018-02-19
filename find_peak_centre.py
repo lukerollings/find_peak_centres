d4cd0ae01af4bc4eec1d3e61ae98d1388ac9e641
lukerollings <33325701+lukerollings@users.noreply.github.com>
2018-02-19 17:17:05 +0000

    Add files via upload
    Now includes method from lmfit algorithm - still doesn't work

--- find_peak_centre.py
+++ find_peak_centre.py
@@ -3,9 +3,10 @@ import numpy as np
 import glob
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
+from lmfit import Model, Parameters
 
 os.chdir('C://Users//mbgnwlr2//Documents//PhD_SynchrotronStuff//Winter18_beamtime_data//xrd//Al_SiC_1D_0_')
-filenames = glob.glob('Al_SiC_1D_0__*.dat')
+filenames = glob.glob('Al_SiC_1D_0__0102.dat')
 
 numfiles = len(filenames)
 
@@ -26,6 +27,12 @@ np.Z = []
 i = 0
 z = -2.98
 
+##generalised 2-factor Gaussian function
+def gauss2(x, a1, b1, c1, a2, b2, c2):
+    return a1*np.exp(-((x-b1)/c1)**2) + a2*np.exp(-((x-b2)/c2)**2)
+
+plt.figure(figsize=(20,10))
+
 for f in filenames:
    
     ##pull data from the file into an array
@@ -43,41 +50,55 @@ for f in filenames:
     
     n = len(x1)
     
-#    plt.plot(x1, y1)
+    plt.plot(x1, y1, 'bo')
     
     peak = max(y1)
     base = min(y1)
     mean = sum(x1*y1)/sum(y1)
     sigma = np.sqrt((sum(y1*(x1 - mean)**2))/sum(y1))
     
-    ##generalised Gaussian function
-    def gauss(x, a1, b1, c1, a2, b2, c2):
-        return a1*np.exp(-((x-b1)/c1)**2) + a2*np.exp(-((x-b2)/c2)**2)
-    
+
+    ##scipy.curve_fit method
     ##fit a curve to Gaussian function, 'popt' returns optimal values for a, b and c
-    popt, pcov = curve_fit(gauss, x1, y1, bounds=([base, (mean-sigma), 0, base, (mean-sigma), 0], [peak, (mean+sigma), 1, peak, (mean+sigma), 1]))
+    popt, pcov = curve_fit(gauss2, x1, y1, bounds=([base, (mean-sigma), 0, base, (mean-sigma), 0], [peak, (mean+sigma), 1, peak, (mean+sigma), 1]))
     
-#    plt.plot(x1, gauss(x1, *popt))
-#    plt.plot([popt[1], popt[1]], [15, 45])
+    plt.plot(x1, gauss2(x1, *popt))
+    plt.plot([popt[1], popt[1]], [15, 45])
+    print(popt)
     
-     
+    
+    ##lmfit.Model method
+    gmodel = Model(gauss2)
+    params = Parameters()
+    params.add_many(('a1', (peak+base)/2, True, base, peak, None, None),
+                    ('b1', mean, True, (mean-sigma), (mean+sigma), None, None),
+                    ('c1', sigma, True, 0, 1, None, None),
+                    ('a2', (peak+base)/2, True, base, peak, None, None),
+                    ('b2', mean, True, (mean-sigma), (mean+sigma), None, None),
+                    ('c2', sigma, True, 0, 1, None, None)
+                    )
+    
+    result = gmodel.fit(y1, params, x=x1)
+    print(result.fit_report())
 
+    plt.plot(x1, result.init_fit)
+    plt.plot(x1, result.best_fit)
     
-    ##Bragg's law: 2*d*sin(theta) = n*lambda
-    deg = (popt[1])/2
-    rad = ((2*np.pi)/360)*deg
-    d = (l/(2*np.sin(rad)))*1e10
-      
-    np.D.insert(i, d)
-    np.Z.insert(i, z)
-    
-    i = i+1
-    z = z+0.02
-
-plt.plot(np.Z, np.D)
-plt.ylim(1.535, 1.545)
-plt.xlabel("z (mm)")
-plt.ylabel("d spacing (Angstrom)")
-#print(np.D)
-#plt.savefig('testgraph.png')
+#    ##Bragg's law: 2*d*sin(theta) = n*lambda
+#    deg = (popt[1])/2
+#    rad = ((2*np.pi)/360)*deg
+#    d = (l/(2*np.sin(rad)))*1e10
+#      
+#    np.D.insert(i, d)
+#    np.Z.insert(i, z)
+#    
+#    i = i+1
+#    z = z+0.02
+#
+#plt.plot(np.Z, np.D)
+#plt.ylim(1.535, 1.545)
+#plt.xlabel("z (mm)")
+#plt.ylabel("d spacing (Angstrom)")
+##print(np.D)
+##plt.savefig('testgraph.png')
     
